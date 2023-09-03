225a3bcfaf2748976b0a565b7b3cbdcd8dbad77f
Dmytro <D-IT@i.ua>
2023-09-03 23:04:33 +0300

    second

---
+++ skript/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const storeName = 'Glory Hole';
+
+const storeDescription = {
+	budget: 10000,
+	employees: ['Roman', 'Vitaliy'],
+	products: {
+		penis: 150,
+		lubricant: 50
+	},
+	open: true,
+};
+
