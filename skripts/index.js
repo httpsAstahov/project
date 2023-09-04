d71e68f0967aec98b7ff2234caf480f6fe908d60
Dmytro <D-IT@i.ua>
2023-09-04 09:58:04 +0300

    hot fix

--- skripts/index.js
+++ skripts/index.js
@@ -11,8 +11,11 @@ const personalMovieDB = {
 };
 
 const lastFilm = prompt('Назовите один из последних просмотренных фильмов:', '');
-const rate = prompt('На сколько оцените его ?:', '');
+const rate = +prompt('На сколько оцените его ?:', '');
+const lastFilmSecond = prompt('Назовите один из последних просмотренных фильмов:', '');
+const rateSecond = +prompt('На сколько оцените его ?:', '');
 
 personalMovieDB.movies[lastFilm] = rate;
+personalMovieDB.movies[lastFilmSecond] = rateSecond;
 
 console.log(personalMovieDB);
